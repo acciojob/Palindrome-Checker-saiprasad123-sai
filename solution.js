794605db826848fd0a6c3efef43aa32f4cabfbae
sai prasad <kondaparthisaiprasad117@gmail.com>
2022-12-15 19:45:34 +0530

    m1

--- solution.js
+++ solution.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
-
+const len=str.length;
+for(let i=0;i<len;i++){
+const l=str[i].toUpperCase();
+const r=str[len-1-i].toUpperCase();
+if(l!==r)
+return false;
+}
+return true;
 }
 module.exports = palindrome
